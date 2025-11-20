64f44d30817621b54d2d2a88ea94f6b2ae4f6d8e
Ritesh Jagga <riteshjagga@gmail.com>
2025-11-20 18:36:10 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
+
+export default defineConfig({
+  plugins: [react(), checker({ typescript: true })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+})
